b747061487f8f27781bce8c6941537e4d2c3c3f7
AuracleTech <VirgilFabre@outlook.com>
2024-01-28 09:39:54 -0500

    Trivial

--- examples/example.rs
+++ examples/example.rs
@@ -1,6 +1,6 @@
-const WIN_TITLE: &'static str = "Vulkan";
-const WIN_INIT_WIDTH: u32 = 1600;
-const WIN_INIT_HEIGHT: u32 = 900;
+const WIN_TITLE: &'static str = env!("CARGO_PKG_NAME");
+const WIN_INIT_WIDTH: u32 = 1280;
+const WIN_INIT_HEIGHT: u32 = 720;
 
 fn main() {
     let _engine = swain::engine(WIN_TITLE, WIN_INIT_WIDTH, WIN_INIT_HEIGHT);
